8ecbfd6e0d6b12b91d05f2afa09115f9bda3d7db
nana <natalia.heuko@escola.pr.gov.br>
2025-06-30 08:12:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('trailer-button').addEventListener('click', function() {
-    window.open('https://www.youtube.com/watch?v=Fh3K0W8hY_Y', '_blank'); // Link do trailer oficial
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página carregada com sucesso!');
 });
